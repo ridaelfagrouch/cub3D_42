34c688f04d838f88cc1ba26ad676d9db2ac6a3ba
Sami Hafid <sahafid@e2r8p13.1337.ma>
2022-09-30 13:02:02 +0100

    add extra layer of protection

--- cub3D_bonus/srcs/executor/srcs/executor.c
+++ cub3D_bonus/srcs/executor/srcs/executor.c
@@ -3,14 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rel-fagr <rel-fagr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sahafid <sahafid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 07:57:18 by sahafid           #+#    #+#             */
-/*   Updated: 2022/09/29 17:00:16 by rel-fagr         ###   ########.fr       */
+/*   Updated: 2022/09/30 12:59:19 by sahafid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../executor.h"
+
+static int	texturevalid(void)
+{
+	int	fd;
+
+	fd = open("srcs/textures/door1.xpm", O_RDONLY);
+	if (fd < 0)
+		return (write(1, "error!! door picture missing file\n", 35), 1);
+	close(fd);
+	fd = open("srcs/textures/door2.xpm", O_RDONLY);
+	if (fd < 0)
+		return (write(1, "error!! door picture missing file\n", 35), 1);
+	close(fd);
+	fd = open("srcs/textures/door3.xpm", O_RDONLY);
+	if (fd < 0)
+		return (write(1, "error!! door picture missing file\n", 35), 1);
+	close(fd);
+	fd = open("srcs/textures/door1.xpm", O_RDONLY);
+	if (fd < 0)
+		return (write(1, "error!! door picture missing file\n", 35), 1);
+	close(fd);
+	fd = open("srcs/textures/chest.xpm", O_RDONLY);
+	if (fd < 0)
+		return (write(1, "error!! chest picture missing file\n", 36), 1);
+	close(fd);
+	return (0);
+}
 
 void	executor(t_map_ *data)
 {
@@ -21,6 +48,8 @@ void	executor(t_map_ *data)
 	init_player(&lst, data);
 	init_raycast(&lst);
 	init_everything(&lst, data);
+	if (texturevalid())
+		return ;
 	init_texture(&lst, data);
 	mlx_loop_hook(lst.mlx, routine, &lst);
 	mlx_loop(lst.mlx);
